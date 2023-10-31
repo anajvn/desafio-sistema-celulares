4adf3b70bc27e7bab453811a9913f80364bdb931
Ana Julia Neiverth <115558556+anajvn@users.noreply.github.com>
2023-10-31 14:29:05 -0300

    Desafio Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Testes com o Nokia
+Nokia nokia = new Nokia("1234-5678", "Nokia G60", "123XYZ", 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+// Testes com o iPhone
+Iphone iphone = new Iphone("1234-5678", "iPhone 15 Pro", "123XYZ", 264);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
